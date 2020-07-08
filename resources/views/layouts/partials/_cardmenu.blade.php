4809854f2c8fe081586a8da148a0c79240d3b9e9
mounkambou-iut-cse-4301 <37999654+mounkambou-iut-cse-4301@users.noreply.github.com>
2020-07-08 09:10:49 +0600

    some modifications

--- resources/views/layouts/partials/_cardmenu.blade.php
+++ resources/views/layouts/partials/_cardmenu.blade.php
@@ -7,7 +7,7 @@
 					<img class="card-img-top" src="img/cse1.jpg" alt="CSE image" style="width:100%; height: 100px;">
 					<div class="card-body">
 						<h4 class="card-title">CSE 1</h4>
-						<a href="/firstyeardetail" class="btn btn-primary">See Details</a>
+						<a href="/firstyeardetail" class="btn" style="background-color:#008B8B">See Details</a>
 					</div>
 				</div><br><br><br>
 			</div>
@@ -16,7 +16,7 @@
 					<img class="card-img-top" src="img/cse2.jpg" alt="CSE image" style="width:100%; height: 100px;">
 					<div class="card-body">
 						<h4 class="card-title">CSE 2</h4>
-						<a href="/secondyeardetail" class="btn btn-primary">See Details</a>
+						<a href="/secondyeardetail" class="btn" style="background-color:#008B8B">See Details</a>
 					</div>
 				</div><br><br><br>
 			</div>
@@ -25,7 +25,7 @@
 					<img class="card-img-top" src="img/cse3.jpg" alt="CSE image" style="width:100%; height: 100px;">
 					<div class="card-body">
 						<h4 class="card-title">CSE 3</h4>
-						<a href="/thirdyeardetail" class="btn btn-primary">See Details</a>
+						<a href="/thirdyeardetail" class="btn" style="background-color:#008B8B">See Details</a>
 					</div>
 				</div><br><br><br>
 			</div>
@@ -34,7 +34,7 @@
 					<img class="card-img-top" src="img/cse4.jpg" alt="CSE image" style="width:100%; height: 100px;">
 					<div class="card-body">
 						<h4 class="card-title">CSE 4</h4>
-						<a href="/fourthyeardetail" class="btn btn-primary">See Details</a>
+						<a href="/fourthyeardetail" class="btn" style="background-color:#008B8B">See Details</a>
 					</div>
 				</div><br><br><br>
 			</div>
